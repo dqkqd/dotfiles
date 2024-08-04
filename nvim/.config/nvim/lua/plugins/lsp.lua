e09bb068cc689fcf46fab98b0b3c0bd92a72c0a0
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-08-04 23:18:46 +0900

    feat: init nvim config

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,54 @@
+return {
+	-- lsp
+	{
+		"neovim/nvim-lspconfig",
+		events = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		opts = {
+			servers = {
+				lua_ls = {},
+			},
+		},
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			for server, server_opts in pairs(opts.servers) do
+				local server_opts_with_capabilities = vim.tbl_deep_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, server_opts)
+				lspconfig[server].setup(server_opts_with_capabilities)
+			end
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"clang-format",
+				"clangd",
+				"lua_ls",
+				"ruff",
+				"selene",
+				"stylua",
+				"shfmt",
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"clang-format",
+				"clangd",
+				"lua_ls",
+				"ruff",
+				"selene",
+				"stylua",
+				"shfmt",
+			},
+		},
+	},
+}
