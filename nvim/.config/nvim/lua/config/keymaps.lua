141684c6db7e4692c8aa3408a59c7bfaee84dd10
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-08-09 06:30:15 +0900

    feat(nvim): overwrite keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+-- formatting
+keymap.set({ "n", "v" }, "<leader>fm", function()
+  LazyVim.format({ force = true })
+end, { desc = "Format" })
