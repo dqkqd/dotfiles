17aa35bedf28a737d1d6c7d632135a624d3b798d
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-08-24 20:59:23 +0900

    feat(nvim): add dap and test

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,8 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.editor.refactoring" },
 
+    { import = "lazyvim.plugins.extras.dap.core" },
+
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
 
@@ -36,6 +38,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
