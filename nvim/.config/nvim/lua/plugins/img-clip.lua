d46012ed03dbb9ac8d14bbe499272b3a536f97c9
Khanh Duong <dqkqdlot@gmail.com>
2026-02-22 20:30:04 +0900

    config img-clip to support custom dir with template for astro

--- nvim/.config/nvim/lua/plugins/img-clip.lua
+++ nvim/.config/nvim/lua/plugins/img-clip.lua
@@ -2,17 +2,45 @@ return {
   "HakonHarnes/img-clip.nvim",
   event = "VeryLazy",
   opts = {
-    default = {
-      dir_path = function()
-        local found = vim.fs.find("assets", {
-          upward = false,
-          type = "directory",
-        })[1]
-        if found then
-          return found
-        end
-        return "assets"
-      end,
+    custom = {
+      {
+        trigger = function() -- returns true to enable
+          local cwd = vim.fn.getcwd()
+          local foldername = vim.fn.fnamemodify(cwd, ":t")
+          return foldername == "blog"
+        end,
+        template = function(ctx)
+          local src = "img_" .. os.date("%s")
+          return string.format(
+            [[
+import %s from "%s";
+
+<Image
+  src={%s}
+  alt="$CURSOR"
+  class="mx-auto"
+/>]],
+            src,
+            ctx.file_path,
+            src
+          )
+        end,
+        dir_path = function()
+          local found = vim.fs.find("assets", {
+            upward = false,
+            type = "directory",
+          })[1]
+          if found then
+            return found
+          end
+          return "assets"
+        end,
+        file_name = function()
+          local name = vim.fn.expand("%:t:r") -- current file without extension
+          local timestamp = os.date("%Y%m%d%H%M%S")
+          return name .. "_" .. timestamp
+        end,
+      },
     },
   },
 }
